acd856d0195c786e5cd403dc4164dcdca45067ef
sam caldwell <mail@samcaldwell.net>
2023-02-25 15:14:49 -0600

    improve messaging for git-server make files and clean up the list output

--- Makefile.d/run.mk
+++ Makefile.d/run.mk
@@ -7,24 +7,30 @@ git_server/initialize:
 REMOTE_IMAGE:=$(DOCKER_HUB)/$(GIT_SERVER_DOCKER_IMAGE_NAME)
 git_server/run: git_server/initialize
 	@echo "$@ starting."
-	docker kill $(GIT_SERVER_DOCKER_CONTAINER_NAME) &> /dev/null || true \
-	docker run -d \
+	@echo "...kill any running container"
+	@docker kill $(GIT_SERVER_DOCKER_CONTAINER_NAME) &> /dev/null || true
+	@echo "...launching container"
+	@docker run -d \
 			   --rm \
 			   --name $(GIT_SERVER_DOCKER_CONTAINER_NAME) \
 			   --publish $(GIT_SERVER_IP_ADDRESS):$(GIT_SERVER_SSH_PORT):22 \
 			   --publish $(GIT_SERVER_IP_ADDRESS):$(GIT_SERVER_HTTP_PORT):80 \
 			   --volume $(GIT_SERVER_VOLUME):/git/ $(REMOTE_IMAGE) .
+	@docker ps
 	@echo "$@ completed."
 
 .$PHONY: git_server/run/local
 LOCAL_IMAGE:=$(GIT_SERVER_DOCKER_IMAGE_NAME)
 git_server/run/local: git_server/initialize
 	@echo "$@ starting."
-	docker kill $(GIT_SERVER_DOCKER_CONTAINER_NAME) &> /dev/null || true \
-	docker run -d \
+	@echo "...kill any running container"
+	@docker kill $(GIT_SERVER_DOCKER_CONTAINER_NAME) &> /dev/null || true
+	@echo "...launching container"
+	@docker run -d \
 			   --rm \
 			   --name $(GIT_SERVER_DOCKER_CONTAINER_NAME) \
 			   --publish $(GIT_SERVER_IP_ADDRESS):$(GIT_SERVER_SSH_PORT):22 \
 			   --publish $(GIT_SERVER_IP_ADDRESS):$(GIT_SERVER_HTTP_PORT):80 \
 			   --volume $(GIT_SERVER_VOLUME):/git/ $(LOCAL_IMAGE) .
+	@docker ps
 	@echo "$@ completed."
